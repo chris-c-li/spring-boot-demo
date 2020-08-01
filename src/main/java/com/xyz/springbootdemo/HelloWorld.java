6d88cc1996fb78822806f59a0b86f6572e6ef674
Cheng Li <chenglii@cs.rutgers.edu>
2020-07-31 20:08:31 -0700

    Initial commit of a spring demo.

---
+++ src/main/java/com/xyz/springbootdemo/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.xyz.springbootdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World\n";
+    }
+}
